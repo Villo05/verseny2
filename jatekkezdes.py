2fcfb81d97eca158171b7c299bc12f24fd15bf90
Villo05 <nemethvillo.2005@gmail.com>
2025-12-09 11:47:23 +0100

    3

--- jatekkezdes.py
+++ jatekkezdes.py
@@ -1,10 +1,23 @@
 def kezdes1():
     karakter_nev=input("Kérem adja meg a karakter nevét: ")
-    print (f"Üdvözöllek {karakter_nev}! Készülj a kalandra!")
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    RESET = "\033[0m"
+
+    def horror_banner():
+        print(RED + BOLD)
+        print("▓" * 80)
+        print(f"             ☠️  Üdvözöllek {karakter_nev}! Készülj a kalandra!  ☠️")
+        print("▓" * 80)
+        print(RESET)
+
     return karakter_nev
 
 
 
 def kezdes2 ():
     print (f"Figyelmeztetés, magánterület! A belépő vállalja, hogy kísérletezzenek rajta.")
+
+
+ 
 
