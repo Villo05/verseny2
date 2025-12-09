5f7637251d0d850cf17bbefcff5ff9e1cfd366fd
wolfstons <paczi.balazs@gmail.com>
2025-12-09 12:05:52 +0100

    szebbkiiras

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ karakter_pozicio=0
 map=["kerítésnél", "postaládánál", "bejáratnál", "konyhában", "hűtőnél", "lépcsőnél", "dolgozóban", "nappaliban", "laborban","börtönben"]
 
 
+RED = "\033[91m"
+BOLD = "\033[1m"
 
+print(RED + BOLD)
 jateknak_vege=ellenorzes.vizsgalat(karakter_pozicio)
-print(150*"*")
+
+print("▓" * 100)
 jatekkezdes.kezdes1()
-print(150*"*")
+print("▓" * 100)
 jatekkezdes.kezdes2()
-print(150*"*")
+print("▓" * 100)
 terkep.map()
-print(150*"*")
-print(150*"*")
+print("▓" * 100)
 while jateknak_vege==False:
     kiiras_helyzet.kiir(karakter_pozicio,map)
-    print(150*"*")
+    print("▓" * 100)
     karakter_pozicio=feladat.lepes(karakter_pozicio,map)
-    print(150*"*")
-    #ellenorzes.vizsgalat(karakter_pozicio)
+    print("▓" * 100)    #ellenorzes.vizsgalat(karakter_pozicio)
     jateknak_vege=ellenorzes.vizsgalat(karakter_pozicio)
 jatekkezdes.befejezes()
